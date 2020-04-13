b8309e267f73b45512a0005e3c56065564f528d6
Ramon Iñiguez <44062098+riniguez91@users.noreply.github.com>
2020-04-13 12:37:22 +0200

    Update pmergesort.py

--- pmergesort.py
+++ pmergesort.py
@@ -48,18 +48,23 @@ def mergeSortParallel(array_to_sort):
 
     # Al terminar, tenemos todos los chunks sorteados, pero nos falta hacer merge() con cada uno para finalizar el algoritmo
     while len(sorted_sublists) > 1: # Mientras haya mas de 1 chunk, significa que todavia no hemos terminado de merge todos los chunks
-        # Hacemos un merge de cada chunk vecino, de tal forma que si tenemos un numero de chunks impar deajmos el ultimo chunk sin tocar para poder 
-        # realizar el ultimo merge de ese chunk con el chunk de todos los merge anteriores, si el numero es par entonces realiza un 
-        # merge de todos los chunks vecinos(de tupla en tupla) hasta que solo quede 1
+        # Hacemos un merge de cada chunk vecino, de tal forma que siempre dejaremos el ultimo chunk sin tocar para poder realizar el ultimo
+        # merge de ese chunk con el chunk de todos los merge anteriores
         chk_lst = [(sorted_sublists[i], sorted_sublists[i+1]) for i in range(0, len(sorted_sublists)-1, 2)] 
         sorted_sublists = pool.map(mergeWrap, chk_lst) # Paralelizamos tambien la operacion mergeWrap(), mirar comentario de la funcion
     
     return sorted_sublists[0] # Todos los chunks se han combinado en una sola lista, por lo que en el primer y unico indice tenemos el resultado
 
 if __name__ == "__main__":
+    print("Generando array con 21,802,766 elementos...")
+    inicio_garray = time.time()
     array_to_sort = [random.randint(0, 30_000) for i in range(21802766)] # Generamos 21_802_766 elementos que varian desde el 0 al 30_000 inclusive
+    fin_garray = time.time()
+    print("Array generado en: {:.2f}s".format(fin_garray-inicio_garray))
     inicio_pmerge = time.time()
+    print("Ordenando array...")
     results = mergeSortParallel(array_to_sort)
     fin_pmerge = time.time()
-    print("Ha tardado en hacer merge sort parallel: ", fin_pmerge-inicio_pmerge)
+    print("Operacion terminada!\nHa tardado en hacer merge sort parallel: {:.2f}s".format(fin_pmerge-inicio_pmerge))
+    print(results[10_000:10_020]) # Imprimos 20 elementos para comprobar resultado
 
